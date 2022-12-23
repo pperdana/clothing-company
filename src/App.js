56b192374498779c70c2e008a36a67cd894e75fa
Panji <panji9502@gmail.com>
2022-12-24 02:59:05 +0700

    add pages and react routes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
